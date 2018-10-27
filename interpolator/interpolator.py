5cfbfe6859e286006241f56bcb867c5a6adecab3
Rik Starmans <info@hexastorm.com>
2018-10-27 17:47:18 +0200

    removed bugs from slicer and fixed sampling errors

--- interpolator/interpolator.py
+++ interpolator/interpolator.py
@@ -25,7 +25,7 @@ class Interpolator:
         # PARAMETERS
         self.tiltangle = np.radians(90)   # angle [radians], for a definition see figure 7
                                           # https://reprap.org/wiki/Transparent_Polygon_Scanning
-        self.laserfrequency = 42720       # the laser frequency [Hz]
+        self.laserfrequency = 42720*8       # the laser frequency [Hz]
         self.rotationfrequency = 2400/60  # rotation frequency polygon [Hz]
         self.facets = 4                   # number of facets
         self.inradius = 15                # inradius polygon [mm]
@@ -33,11 +33,16 @@ class Interpolator:
         self.pltfxsize = 200              # platform size scanning direction [mm]
         self.pltfysize = 200              # platform size stacking direction [mm]
         self.samplexsize = 0              # sample size scanning direction [mm]
-        self.sampleysize = 0              # sample size stacking direction [mm] 
-        self.samplegridsize = 0.05        # height/width of the sample gridth [mm]
+        self.sampleysize = 0              # sample size stacking direction [mm]
+        # NOTE: sample below Nyquist criterion
+        #       you first sample the image, you then again sample the sampled image 
+        self.samplegridsize = 0.015       # height/width of the sample gridth [mm]
         self.stagespeed = 4.8             # mm/s
-        self.startpixel = 10              # pixel determine via camera
-        self.pixelsinline = 230           # number of pixels in a line
+        self.startpixel = 10*8            # pixel determine via camera
+        self.pixelsinline = 230*8         # number of pixels in a line
+        # debug folder
+        currentdir = os.path.dirname(os.path.realpath(__file__))
+        self.debug_folder = os.path.join(currentdir, 'debug')
 
 
     def pstoarray(self, url):
@@ -47,7 +52,7 @@ class Interpolator:
         '''
         psppoint = 0.3527777778 # post script pixel in mm
         tmp = Image.open(url)
-        x_size, y_size = [i*psppoint for i in tmp.size]
+        y_size, x_size = [i*psppoint for i in tmp.size]
         if x_size > self.pltfxsize or y_size > self.pltfysize:
             raise Exception('Object does not fit on platform')
         #NOTE: this is only a crude approximation
@@ -97,7 +102,8 @@ class Interpolator:
         '''
         returns the laserdiode y-position in pixels
 
-        The y-asis is orthogonal to the scanline if the stage does not move.
+        The y-axis is orthogonal to the scanline if the stage does not move,
+        and parallel to the stage movement.
         :param pixel: the pixelnumber in the line
         :param direction: True is +, False is -
         :param ystart: the y-start position [mm]
@@ -130,7 +136,7 @@ class Interpolator:
         vfxpos = np.vectorize(self.fxpos)
         vfypos = np.vectorize(self.fypos)
         xstart = self.fxpos(self.pixelsinline-1)*self.samplegridsize
-        # you still don't account for ystart
+        # TODO: you still don't account for ystart
         xpos_facet = vfxpos(range(0, self.pixelsinline), xstart)
         ypos_forwardfacet = vfypos(range(0, self.pixelsinline), True)
         ypos_backwardfacet = vfypos(range(0, self.pixelsinline), False)
@@ -149,42 +155,45 @@ class Interpolator:
         ypos = []
         for lane in range(0, lanes):
             xpos_temp = xpos_lane + lane * (self.fxpos(0)-self.fxpos(self.pixelsinline-1))
-            if lanes % 2 == 1:
+            if lane % 2 == 1:
                 ypos_temp = ypos_backwardlane
             else:
                 ypos_temp = ypos_forwardlane
             xpos += xpos_temp.tolist()
             ypos += ypos_temp
-        #NOTE: float leads to additional patterns in the final slice
-        print(max(xpos))
-        print(max(ypos))
-        ids = np.concatenate(([np.array(xpos)], [np.array(ypos)]))
+        # interpolation is linear, but in the end you convert to binary so can be converted to int as well.
+        xpos = np.array(xpos)
+        ypos = np.array(ypos)
+        ids = np.concatenate(([ypos], [xpos]))
         return ids
 
 
-    def patternfile(self, url):
+    def patternfile(self, url, test=False):
         '''returns the pattern file as numpy array
 
         mostly a convenience function which wraps other functions in this class
         :param url: path to postscript file
+        :param test: runs a sampling test, whether laser frequency sufficient to provide accurate sample
         '''
         from time import time
         ctime = time()
-        layerarr = self.pstoarray(url)
+        layerarr = self.pstoarray(url).astype(np.uint8)
+        if test:
+            img = Image.fromarray(layerarr.astype(np.uint8)*255)
+            img.save(os.path.join(self.debug_folder, 'nyquistcheck.png'))
+        if test: 
+            layerarr = np.ones_like(layerarr)
         print("Retrieved layerarr")
         print("elapsed {}".format(time()-ctime))
         ids = self.createcoordinates()
         print("Retrieved coordinates")
         print("elapsed {}".format(time()-ctime))
-        #TODO: test
-        # values outside image are mapped to 0
-        print(layerarr.min())
-        print(layerarr.max())
-        print(layerarr.shape)
-        ptrn = ndimage.map_coordinates(input=layerarr, output=np.uint8, coordinates=ids, order=1, mode="constant", cval=0)
-        print(ptrn.min())
-        print(ptrn.max())
-        # max array is set to one
+        if test:
+            ptrn = ndimage.map_coordinates(input=layerarr, output=np.uint8, 
+            coordinates=ids, order=1, mode="constant", cval=1)
+        else:
+            ptrn = ndimage.map_coordinates(input=layerarr, output=np.uint8, 
+            coordinates=ids, order=1, mode="constant", cval=0)
         print("Completed interpolation")
         print("elapsed {}".format(time()-ctime))
         print("Final shape {}".format(ptrn.shape))
@@ -200,48 +209,9 @@ class Interpolator:
         :param step: pixel step, can be used to lower the number of pixels that are plotted 
         :param filename: filename to store pattern
         '''
-        currentdir = os.path.dirname(os.path.realpath(__file__))
-        # the positions are constructed
-        vfxpos = np.vectorize(self.fxpos) 
-        vfypos = np.vectorize(self.fypos)
-
-        # NOTE: coordinates are created similarly to create coordinates
-        lanewidth = (self.fxpos(0)-self.fxpos(self.pixelsinline-1))*self.samplegridsize  # mm
-        lanes = math.ceil(self.samplexsize/lanewidth)
-        facets_inlane = math.ceil(self.rotationfrequency * self.facets * (self.sampleysize/self.stagespeed))
-        # single facet
-        vfxpos = np.vectorize(self.fxpos)
-        vfypos = np.vectorize(self.fypos)
-        xstart = self.fxpos(self.pixelsinline-1) #TODO: make sure you account for this in your movement
-        xpos_facet = vfxpos(range(0, self.pixelsinline), xstart)
-        ypos_forwardfacet = vfypos(range(0, self.pixelsinline), True)
-        ypos_backwardfacet = vfypos(range(0, self.pixelsinline), False)
-        # single lane
-        xpos_lane = xpos_facet.tolist() * facets_inlane
-        ypos_forwardlane = []
-        ypos_backwardlane = []
-        for facet in range(0, facets_inlane):
-            ypos_forwardtemp = ypos_forwardfacet + (facet*self.stagespeed)/(self.facets*self.rotationfrequency*self.samplegridsize)
-            ypos_backwardtemp = ypos_backwardfacet + ((facets_inlane-facet)*self.stagespeed)/(self.facets*self.rotationfrequency*self.samplegridsize)
-            ypos_forwardlane += ypos_forwardtemp.tolist()
-            ypos_backwardlane += ypos_backwardtemp.tolist()
-        # all lanes
-        xpos_lane = np.array(xpos_lane)
-        xpos = []
-        ypos = []
-        for lane in range(0, lanes):
-            xpos_temp = xpos_lane + lane * (self.fxpos(0)-self.fxpos(self.pixelsinline-1))
-            if lanes % 2 == 1:
-                ypos_temp = ypos_backwardlane
-            else:
-                ypos_temp = ypos_forwardlane
-            xpos += xpos_temp.tolist()
-            ypos += ypos_temp
-        #NOTE: end note
-        # if this is not done, operation becomes too slow
-        # conversion to int needed, as array indices must be integer
-        xcor = np.array(xpos[::step]).astype(np.int32, copy=False)
-        ycor = np.array(ypos[::step]).astype(np.int32, copy=False)
+        ids = self.createcoordinates()
+        xcor = np.array(ids[1, ::step]).astype(np.int32)
+        ycor = np.array(ids[0, ::step]).astype(np.int32)
         # x and y cannot be negative
         if xcor.min()< 0:
             xcor += abs(xcor.min())
@@ -250,10 +220,9 @@ class Interpolator:
         # number of pixels ptrn.shape[0]
         arr = np.zeros((ycor.max() + 1, xcor.max() + 1), dtype=np.uint8)
         arr[ycor[:], xcor[:]] = ptrn[0 : len(ptrn): step]
-        #img[:,113]=1 #the line is at 113
         arr = arr * 255
         img = Image.fromarray(arr)
-        img.save(filename + '.png')
+        img.save(os.path.join(self.debug_folder, filename + '.png'))
         return img
 
 
@@ -263,8 +232,7 @@ class Interpolator:
 
         :param name
         '''
-        currentdir = os.path.dirname(os.path.realpath(__file__))
-        pat=np.fromfile(os.path.join(currentdir, name), dtype=np.uint8)
+        pat=np.fromfile(os.path.join(self.debug_folder, name), dtype=np.uint8)
         return pat
 
 
@@ -275,15 +243,13 @@ class Interpolator:
         :param pixeldata must have uneven length
         :param filename name of file
         '''
-        currentdir = os.path.dirname(os.path.realpath(__file__))
         pixeldata=pixeldata.astype(np.uint8)
-        pixeldata.tofile(os.path.join(currentdir, filename))
+        pixeldata.tofile(os.path.join(self.debug_folder, filename))
 
 
-#TODO: the coordinates had a different type in the original --> np.int32
-# a line resutts from / or // in return
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":    
     interpolator = Interpolator()
     dir_path = os.path.dirname(os.path.realpath(__file__))
     url = os.path.join(dir_path, 'test-patterns', 'line-resolution-test.ps')
@@ -291,5 +257,4 @@ if __name__ == "__main__":
     interpolator.writebin(ptrn, "test.bin")
     pat = interpolator.readbin("test.bin")
     print("The shape of the pattern is {}".format(pat.shape))
-    # testing the slices can only be done at 64 bit, VTK is however installed for 32 bit
     interpolator.plotptrn(pat, 1)
